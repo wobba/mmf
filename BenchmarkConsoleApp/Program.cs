56c934aa02c03c140e7ad347fc553f8bdeb68a36
Mikael Svenson <51104+wobba@users.noreply.github.com>
2010-01-03 17:05:11 +0000

    Added Benchmark console app.

---
+++ BenchmarkConsoleApp/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using mAdcOW.DataStructures;
+using mAdcOW.DataStructures.DictionaryBacking;
+
+namespace BenchmarkConsoleApp
+{
+    internal class Program
+    {
+        private static int MaxCount = 1000000;
+
+        public static int Main(string[] args)
+        {
+            ThreadPool.SetMaxThreads(10, 1000);
+
+            //TextWriterTraceListener tr1 = new TextWriterTraceListener(Console.Out);
+            //Debug.Listeners.Add(tr1);
+
+            SingelThread_HashInMemory();
+            SingelThread_HashOnDisk();
+            Threaded_HashInMemory();
+            Threaded_HashOnDisk();
+            return 0;
+        }
+
+        private static void Threaded_HashOnDisk()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            BackingUnknownSize<string, string> backingFile = new BackingUnknownSize<string, string>(path, MaxCount);
+
+            Dictionary<string, string> dict = new Dictionary<string, string>(backingFile);
+
+            Console.WriteLine("Queuing {0} items to Thread Pool", MaxCount);
+            Console.WriteLine("Queue to Thread Pool 0");
+            System.Collections.Generic.List<WaitHandle> handles = new System.Collections.Generic.List<WaitHandle>();
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int iItem = 1; iItem < 20; iItem++)
+            {
+                ManualResetEvent mre = new ManualResetEvent(false);
+                handles.Add(mre);
+                ThreadPool.QueueUserWorkItem(d =>
+                                                 {
+                                                     for (int i = 0; i < MaxCount/20; i++)
+                                                     {
+                                                         string key = Guid.NewGuid().ToString();
+                                                         dict.Add(key, key);
+                                                         string x = dict[key];
+                                                         Trace.WriteLine(x.Length);
+                                                     }
+                                                     mre.Set();
+                                                 }, null);
+            }
+            Console.WriteLine("Waiting for Thread Pool to drain");
+            WaitHandle.WaitAll(handles.ToArray());
+            sw.Stop();
+            Console.WriteLine("Thread Pool has been drained (Event fired)");
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        private static void Threaded_HashInMemory()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            DictionaryPersist<string, string> backingFile = new DictionaryPersist<string, string>(path, MaxCount);
+
+            Dictionary<string, string> dict = new Dictionary<string, string>(backingFile);
+
+            Console.WriteLine("Queuing {0} items to Thread Pool", MaxCount);
+            Console.WriteLine("Queue to Thread Pool 0");
+            System.Collections.Generic.List<WaitHandle> handles = new System.Collections.Generic.List<WaitHandle>();
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int iItem = 1; iItem < 20; iItem++)
+            {
+                ManualResetEvent mre = new ManualResetEvent(false);
+                handles.Add(mre);
+                ThreadPool.QueueUserWorkItem(d =>
+                                                 {
+                                                     for (int i = 0; i < MaxCount/20; i++)
+                                                     {
+                                                         string key = Guid.NewGuid().ToString();
+                                                         dict.Add(key, key);
+                                                         string x = dict[key];
+                                                         Trace.WriteLine(x.Length);
+                                                     }
+                                                     mre.Set();
+                                                 }, null);
+            }
+            Console.WriteLine("Waiting for Thread Pool to drain");
+            WaitHandle.WaitAll(handles.ToArray());
+            sw.Stop();
+            Console.WriteLine("Thread Pool has been drained (Event fired)");
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        private static void SingelThread_HashOnDisk()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            BackingUnknownSize<string, string> backingFile = new BackingUnknownSize<string, string>(path, MaxCount);
+
+            Dictionary<string, string> dict = new Dictionary<string, string>(backingFile);
+
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < MaxCount; i++)
+            {
+                string key = Guid.NewGuid().ToString();
+                dict.Add(key, key);
+                string x = dict[key];
+                Trace.WriteLine(x.Length);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        private static void SingelThread_HashInMemory()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            DictionaryPersist<string, string> backingFile = new DictionaryPersist<string, string>(path, MaxCount);
+
+            Dictionary<string, string> dict = new Dictionary<string, string>(backingFile);
+
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < MaxCount; i++)
+            {
+                string key = Guid.NewGuid().ToString();
+                dict.Add(key, key);
+                string x = dict[key];
+                Trace.WriteLine(x.Length);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+    }
+}
