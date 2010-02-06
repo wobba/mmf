dbd904490467b5e78979f68e9a3dc1ee7df8beb3
Mikael Svenson <51104+wobba@users.noreply.github.com>
2010-02-06 14:38:36 +0000

    Rename.

--- Tests/mAdcOW.DataStructures.Test/Properties/AssemblyInfo.cs
+++ Tests/mAdcOW.DataStructures.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DiskStructuresTest")]
+[assembly: AssemblyTitle("DataStructuresTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DiskStructuresTest")]
+[assembly: AssemblyProduct("DataStructuresTest")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
