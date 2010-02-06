e763d46ca22512f234e5d963407cdb58b9cdf16c
Mikael Svenson <51104+wobba@users.noreply.github.com>
2010-02-06 14:27:42 +0000

    Improved test.

--- Tests/mAdcOW.DataStructures.Test/RecursiveTest.cs
+++ Tests/mAdcOW.DataStructures.Test/RecursiveTest.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace mAdcOW.DataStructures.Test
@@ -12,13 +10,11 @@ namespace mAdcOW.DataStructures.Test
     [TestClass]
     public class RecursiveTest
     {
-        public class cSandboxFileInfo
+        public class CSandboxFileInfo
         {
             public string La { get; set; }
             public string Lu { get; set; }
         }
-
-        private TestContext testContextInstance;
 
         /// <summary>
         ///Gets or sets the test context which provides
@@ -28,23 +24,24 @@ namespace mAdcOW.DataStructures.Test
 
 
         [TestMethod]
-        public void TestMethod1()
+        public void Accessing_collection_while_iterating_should_not_thow_exception()
         {
             string path = AppDomain.CurrentDomain.BaseDirectory;
-            DataStructures.Dictionary<string, cSandboxFileInfo> dictSandboxFileInfo = new Dictionary<string, cSandboxFileInfo>(path, 1000);
-            cSandboxFileInfo c = new cSandboxFileInfo {La = "lalalalala", Lu = "lululululu"};
+            Dictionary<string, CSandboxFileInfo> dictSandboxFileInfo = new Dictionary<string, CSandboxFileInfo>(path,
+                                                                                                                1000);
+            CSandboxFileInfo c = new CSandboxFileInfo {La = "lalalalala", Lu = "lululululu"};
             dictSandboxFileInfo.Add("somekey", c);
 
 
-            DataStructures.Dictionary<string, string> keyDict = new Dictionary<string, string>(path, 1000);
+            Dictionary<string, string> keyDict = new Dictionary<string, string>(path, 1000);
             KeyValuePair<string, string> kvp = new KeyValuePair<string, string>("somekey", "bbbb");
             keyDict.Add(kvp);
 
             foreach (KeyValuePair<string, string> deSandboxFile in keyDict)
             {
                 var val = dictSandboxFileInfo[deSandboxFile.Key];
-            }            
-
+                var obj = keyDict[deSandboxFile.Key];
+            }
         }
     }
 }
